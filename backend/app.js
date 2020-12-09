5ce844d4832ef57a8d162f72178b39644bc1c1b6
Johnson Zhou <johnson.zhou.sh@icloud.com>
2020-12-10 01:25:53 +0800

    added CORS

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,11 @@ app.use('/api', jwt({
 }));
 app.use('/api/myProfile', require('./routes/myProfile'))
 app.use('/api/classlist', require('./routes/classlist'))
+app.use('/api', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.static(path.join(__dirname, "../frontend/egglenderlogin", "build")));
 // app.use(express.static("public"));
@@ -60,7 +65,9 @@ app.use('/api', (err, req, res, next) => {
 
 // app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -72,19 +79,19 @@ app.use('/api/login', require('./routes/login'))
 app.use('/api/setting', require('./routes/setting'))
 app.use('/api/settingupdate', require('./routes/settingUpdate'))
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-//   res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    //   res.render('error');
 });
 
 module.exports = app;
