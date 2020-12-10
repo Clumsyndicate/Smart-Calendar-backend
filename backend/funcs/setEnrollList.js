9395991076cc92aaea9017d9fe2d61fb3c587ca7
Johnson Zhou <johnson.zhou.sh@icloud.com>
2020-12-10 23:33:34 +0800

    bug fix

--- backend/funcs/setEnrollList.js
+++ backend/funcs/setEnrollList.js
@@ -1,7 +1,7 @@
 const database = require('../database')
 
 module.exports = (username, newlist, contact, callback) => {
-    const sql1 = `UPDATE users SET classes='${JSON.stringify({"array": newlist})}', contact='${JSON.stringify(contact)}' WHERE userName='${username}'`;
+    const sql1 = `UPDATE users SET classes='${JSON.stringify(newlist)}', contact='${JSON.stringify(contact)}' WHERE userName='${username}'`;
     database(sql1, username, result => {
         callback(result.changedRows === 0);
     });
