f07ac6a12cc16d32a3e6ce9677770e4a5fc96c5d
Johnson Zhou <johnson.zhou.sh@icloud.com>
2020-12-15 00:25:29 +0800

    Bug fixes

--- backend/funcs/getEnrollList.js
+++ backend/funcs/getEnrollList.js
@@ -3,7 +3,7 @@ const database = require('../database')
 module.exports = (username, callback) => {
     const sql = 'SELECT * FROM users WHERE userName=?';
     database(sql, username, result => {
-        if (result.length !== 1 && result[0].classes) {
+        if (result.length === 1 && result[0].classes) {
             callback(JSON.parse(result[0].classes), JSON.parse(result[0].contact), result[0].avatar);
         } else {
             callback([], null, null);
